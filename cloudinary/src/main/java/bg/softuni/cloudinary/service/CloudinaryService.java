dab416bb689243683ea4a7f53b43e3866a3e4374
Valeri Totev <valerivt@yahoo.com>
2021-11-08 07:51:48 +0200

    update

---
+++ cloudinary/src/main/java/bg/softuni/cloudinary/service/CloudinaryService.java
@@ -0,0 +1,10 @@
+package bg.softuni.cloudinary.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface CloudinaryService {
+    CloudinaryImage upload(MultipartFile multipartFile) throws IOException;
+    boolean delete(String publicId);
+}
